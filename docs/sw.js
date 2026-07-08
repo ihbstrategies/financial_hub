53ad6ce4ea2cf644051dc2bc200d925bc08c44b5
doctutomika <ihb@ihbstrategies.com>
2026-07-08 17:39:32 -0300

    Mobile iPhone: safe-area no topbar/sidebar (não vaza no notch) + trava pinch-zoom

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const C='fh-shell-1783541487';const SHELL=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./favicon.svg','./favicon-32.png','./apple-touch-icon.png'];
+const C='fh-shell-1783543172';const SHELL=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./favicon.svg','./favicon-32.png','./apple-touch-icon.png'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(SHELL);}).then(function(){return self.skipWaiting();}));});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.map(function(k){if(k!==C)return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
 self.addEventListener('fetch',function(e){var u=new URL(e.request.url);if(e.request.method!=='GET'||u.origin!==location.origin)return;e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request).then(function(resp){var cp=resp.clone();caches.open(C).then(function(c){c.put(e.request,cp);});return resp;}).catch(function(){return caches.match('./index.html');});}));});
